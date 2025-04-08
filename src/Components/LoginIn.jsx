333c37039d58b6b2b360d0ee098d90114db16608
ParasP41 <parasvishwakarma198@gmail.com>
2025-04-08 12:21:12 +0530

    to-do-app

--- src/Components/LoginIn.jsx
+++ src/Components/LoginIn.jsx
@@ -45,16 +45,16 @@ function LoginIn() {
   };
 
   return (
-    <div className="flex my-40 item-center justify-center">
-      <form className="bg-gray-300 w-120 p-10 rounded-2xl">
+    <div className="flex my-28 item-center justify-center">
+      <form className="bg-gray-300 w-120 p-10 m-5 rounded-2xl">
        
-        <div className="flex justify-between items-end">
+        <div className="flex justify-between  items-center my-5 md:my-2 ">
           <div className="text-4xl font-light md:my-4">Sign Up</div>
 
          
           <Link
             to="/login"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center"
           >
             Log In
           </Link>
@@ -115,7 +115,7 @@ function LoginIn() {
       
         <button
           onClick={handlerSignUp}
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center"
         >
           Sign Up
         </button>
--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -37,14 +37,14 @@ function SignUp() {
   };
 
   return (
-    <div className="flex my-40 item-center justify-center">
-      <form className="bg-gray-300 w-120 p-10 rounded-2xl">
+    <div className="flex my-33 item-center justify-center">
+      <form className="bg-gray-300 w-120 p-10 m-5 rounded-2xl">
        
-        <div className="flex justify-between items-end">
-          <div className="text-4xl font-light md:my-4">Log In</div>
+        <div className="flex justify-between items-center my-5 md:my-2 ">
+          <div className=" md:text-4xl text-3xl font-light md:py-4 ">Log In</div>
           <Link
             to="/signup"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center"
           >
             Sign Up
           </Link>
@@ -79,7 +79,7 @@ function SignUp() {
         
         <button
           onClick={handlerLogin}
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center"
         >
           Log In
         </button>
